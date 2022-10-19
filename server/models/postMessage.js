386e35177a22a87f26611474420de96648ee24aa
Hasnat Bajwa <hasnatjbajwa@gmail.com>
2022-10-19 14:15:13 +0500

    initialized backend

---
+++ server/models/postMessage.js
@@ -0,0 +1,21 @@
+// creating database schema
+import mongoose  from "mongoose";
+const postSchema = mongoose.Schema({
+title: String,
+message: String,
+creator: String,
+tags: [String],
+likeCount: {
+    type: Number,
+    default: 0
+},
+createdAt:{
+type: Date,
+default: new Date()
+}
+});
+
+// creating model
+
+const PostMessage = mongoose.model('PostMessage',postSchema);
+export default PostMessage;
